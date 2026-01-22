6a1f57b79dd88dfa18c7b7e0cb60a0bc217a92c7
Ahmed-Yehia-Sokar <ahmedyehiasokar94@gmail.com>
2026-01-22 12:16:52 +0200

    Initial Commit

---
+++ AroundEgypt/AroundEgyptApp.swift
@@ -0,0 +1,17 @@
+//
+//  AroundEgyptApp.swift
+//  AroundEgypt
+//
+//  Created by Admin on 22/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct AroundEgyptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
